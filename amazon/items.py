ab806f7d5f7737bcb83dcee2d62ef802c4b4e51e
salihm08 <salihm08@gmail.com>
2017-10-12 21:04:22 +0530

    added base scrapy project for amazon

---
+++ amazon/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class AmazonItem(scrapy.Item):
+    pnme = scrapy.Field()
+    descp = scrapy.Field()
+    price = scrapy.Field()
+    img = scrapy.Field()
---
+++ amazon/spiders/amazon1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from amazon.items import AmazonItem
+class Amazon1Spider(scrapy.Spider):
+    name = 'amazon1'
+    allowed_domains = ['amazon.in']
+    start_urls = ['https://www.amazon.in/b?node=4510749031']
+
+    def parse(self, response):
+        LINK = "//a[@class='a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal']//@href"
+        plink = response.xpath(LINK).extract()
+        for link in plink :
+        	yield scrapy.Request(url=link,callback=self.parse_product)
+        NEXT ="//a[@class='pagnNext']//@href"
+        next = response.xpath(NEXT).extract_first()
+        next = 'https://www.amazon.in'+next if next else None
+        if next is not None:
+        	yield scrapy.Request(url=next,callback=self.parse)
+
+    def parse_product(self,response):
+    	PNME = "//span[@id='productTitle']/text()"
+    	pnme = response.xpath(PNME).extract()
+    	pnme = [a.strip() for a in pnme]
+    	pnme = "".join(pnme)
+    	DESCP = "//div[@id='feature-bullets']/ul/li/span/text()"
+    	descp = response.xpath(DESCP).extract()
+    	descp = [a.strip() for a in descp]
+    	descp = "".join(descp)
+    	PRICE = "//span[@id='priceblock_ourprice']/text()"
+    	price = response.xpath(PRICE).extract()
+    	IMG = "//img[@id='landingImage']//@src"
+    	img = response.xpath(IMG).extract()
+    	yield AmazonItem(pnme=pnme,descp=descp,price=price,img=img)
